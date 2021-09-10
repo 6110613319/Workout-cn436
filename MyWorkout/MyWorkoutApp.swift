60abe269c9e7cb7fee6f55c9b4d0a0587ab2b6a2
Mook <natthaporn.wim@dome.tu.ac.th>
2021-09-10 10:20:53 +0700

    Initial Commit

---
+++ MyWorkout/MyWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutApp.swift
+//  MyWorkout
+//
+//  Created by Natthaporn Wimonanupong on 10/9/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
